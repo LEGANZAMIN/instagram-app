dc60a6bc9a88e265ed6f6f8bca53e07b8045b507
LeeSungmin <lsmin01@gmail.com>
2020-02-08 01:45:51 +0900

    apollo client

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { Ionicons } from "@expo/vector-icons";
-import { Text, View } from "react-native";
+import { Text, View, AsyncStorage } from "react-native";
+
 import { AppLoading } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import options from "./apollo";
+
 export default function App() {
     const [loaded, setLoaded] = useState(false);
+    const [client, setClient] = useState(null);
 
     const preLoad = async () => {
         try {
+            // Font
             await Font.loadAsync({
                 ...Ionicons.font
             });
 
+            // Images
             Asset.loadAsync([require("./assets/logo.png")]);
 
+            // Apollo client
+            const cache = new InMemoryCache();
+            await persistCache({
+                cache,
+                storage: AsyncStorage
+            });
+
+            const client = new ApolloClient({
+                cache,
+                ...options
+            });
+
             setLoaded(true);
+            setClient(client);
         } catch (e) {
             console.log(e);
         }
@@ -25,13 +48,14 @@ export default function App() {
 
     useEffect(() => {
         preLoad();
-        //setLoaded(false);
     }, []);
 
-    return loaded ? (
-        <View>
-            <Text>This is Sample App...</Text>
-        </View>
+    return loaded && client ? (
+        <ApolloProvider client={client}>
+            <View>
+                <Text>This is Sample App...</Text>
+            </View>
+        </ApolloProvider>
     ) : (
         <AppLoading />
     );
---
+++ apollo.js
@@ -0,0 +1,5 @@
+const options = {
+    url: "http://localhost:4000/graphql"
+};
+
+export default options;
